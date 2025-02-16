d15fe11abe7c46925a71de1b7c460e744aceb813
Juan Pablo Villa Serna <jpvilla1990@gmail.com>
2025-02-16 16:01:39 +0100

    Refactoring code to make it reusable as a pip package in different projects

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='ChatTime',
-    version='0.1',
-    packages=find_packages(where='.'),  # Look for packages in the current directory (ChatTime)
-    package_dir={'': '.'},  # Treat the current directory (ChatTime) as the root package
-)
